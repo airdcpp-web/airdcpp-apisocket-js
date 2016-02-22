04f8cd0ed1a2f076377f950989ce4a45f128cb76
maksis <maksis@adrenaline-network.com>
2016-02-22 18:05:39 +0200

    ShareChecker example: search forbidden files from own share as well

--- examples/ShareChecker.js
+++ examples/ShareChecker.js
@@ -21,6 +21,9 @@ const searchItems = [
 	}
 ];
 
+const checkOwnShare = true;
+const checkConnectingUsers = true;
+
 // CONFIG END
 
 
@@ -29,26 +32,32 @@ const ApiSocket = require('../').default;
 const socket = ApiSocket(require('./settings'));
 
 
-const onResultsReceived = (user, item, results) => {
+const formatResultPaths = (results) => {
+	const paths = results.reduce((reduced, result) => {
+		reduced.push(result.path);
+		return reduced;
+	}, []);
+
+	return paths.join(', ');
+};
+
+const onUserResultsReceived = (user, item, results) => {
 	if (results.length === 0) {
 		return;
 	}
-
-	// Only report for a single result
-	const result = results[0];
 
 	// Notify the user
 	// Forbidden item(s) found from share: ISO file(s) over 500 MB (example: /Linux/Ubuntu 15.04.iso)
 	socket.post('private_chat/v0/message', {
 		user: user,
-		text: 'Forbidden item(s) found from share: ' + item.description + ' (example: ' + result.path + ')'
+		text: 'Forbidden item(s) found from share: ' + item.description + ' (' + formatResultPaths(results) + ')'
 	});
 
 	// Show a status message in that particular hub
 	// [100]MrFastSpeed: ISO file(s) over 500 MB (/Linux/Ubuntu 15.04.iso)
 	socket.post('hubs/v0/status', {
 		hub_urls: [ user.hub_url ],
-		text: user.nick + ': ' + item.description + ' (' + result.path + ')',
+		text: user.nick + ': ' + item.description + ' (' + formatResultPaths(results) + ')',
 		severity: 'info',
 	});
 };
@@ -65,12 +74,42 @@ const onUserConnected = (user) => {
 			user: user,
 			query: item.query
 		})
-			.then(onResultsReceived.bind(this, user, item));
+			.then(onUserResultsReceived.bind(this, user, item));
+	});
+};
+
+const onShareResultsReceived = (item, results) => {
+	if (results.length === 0) {
+		return;
+	}
+
+	// Show an event message
+	// Forbidden item found from own share: ISO file(s) over 500 MB (/Linux/Ubuntu 15.04.iso)
+	socket.post('events/v0/message', {
+		text: 'Forbidden item found from own share' + ': ' + item.description + ' (' + formatResultPaths(results) + ')',
+		severity: 'info',
+	});
+};
+
+const searchOwnShare = () => {
+	// This will only search from the default profile
+	searchItems.forEach((item) => {
+		socket.post('search/v0/query/share', {
+			query: item.query
+		})
+			.then(onShareResultsReceived.bind(this, item));
 	});
 };
 
 socket.onConnected = () => {
-	socket.addListener('hubs/v0', 'hub_user_connected', onUserConnected);
+	if (checkConnectingUsers) {
+		socket.addListener('hubs/v0', 'hub_user_connected', onUserConnected);
+	}
+
+	if (checkOwnShare) {
+		// Check own share on every connect
+		searchOwnShare();
+	}
 };
 
 socket.connect();
